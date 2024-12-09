1d9de7ae7e6159e48de72c2075b0874ae4a30e60
YusolCho <112920170+YusolCho@users.noreply.github.com>
2024-12-10 00:51:29 +0900

    fix: solved problem 1210

--- BJ_20207_달력.py
+++ BJ_20207_달력.py
@@ -1,56 +1,32 @@
-N = int(input())
-table = [] # 
-for _ in range(N):
-    s, e = map(int, input().split())
-    l = s - e 
-    table.append([s,l,e]) # 약속 시작 날짜, -(약속길이)
-table.sort() # 약속 시작 날짜가 이르고 약속 길이가 긴 순서대로 정렬
+import sys
+input = sys.stdin.read
 
-# 약속이 있는 마지막 날짜
-max_value = 0
-for meeting in table:
-    max_value = max(max_value, meeting[2])
+data = input().split()
+N = int(data[0])
+table = [(int(data[i]), int(data[i + 1])) for i in range(1, len(data), 2)]
+table.sort()
 
-# 약속 표시할 캘린더 생성(약속이 있는 마지막 날짜 x 모든 약속의 개수)
-graph = [[0] * max_value for _ in range(N)]
+max_value = max(e for _, e in table)
+graph = [0] * max_value
 
-# 캘린더에 약속 표시 
-def fill_calendar(meeting):
-    global graph
-    s, e = meeting[0]-1 , meeting[2]-1
+for s, e in table:
+    for i in range(s - 1, e):
+        graph[i] += 1
 
-    q = [0]
-    while q: # 각 층에 대해서 
+# 총면적 계산
+total_area, i = 0, 0
+while i < max_value:
+    if graph[i] == 0:
+        i += 1
+        continue
 
-        floor = q.pop()
-        if floor >= len(graph):
-            return
-        
-        if sum(graph[floor][s:e+1]) == 0: # 그래프의 i층의 시작~끝 인덱스가 만약 다 0이라면
-            graph[floor][s:e+1] = [1]*(len(graph[floor][s:e+1])) # 1로 채운 다음 
-            return # 중단
-        else:
-            q.append(floor+1)
+    start = i
+    max_height = 0
+    while i < max_value and graph[i] > 0:
+        max_height = max(max_height, graph[i])
+        i += 1
 
-# 모든 미팅에 대해 적용
-for meeting in table:
-    fill_calendar(meeting)
+    total_area += (i - start) * max_height
 
-# 각 날짜별 약속 개수
-column_sum = [sum(row) for row in zip(*graph)]+[0]
-
-# 직사각형의 높이, 너비, 그리고 총 넓이 변수 지정
-height, width, total_area = 0, 0, 0
-
-# 넓이 계산 
-for i in column_sum:
-    if i !=0:
-        width += 1
-        height = max(height, i)
-    
-    if i == 0:
-        total_area += width*height
-        width, height = 0, 0
-
-# 정답        
-print(total_area)
+# 답 출력
+print(total_area) 
